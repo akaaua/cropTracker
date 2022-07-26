5e26c51e5552a2a72c2408869d3d4e5d7eba421e
akaaua <akaaua@gmail.com>
2022-07-26 08:46:59 -0400

    Salvando itens em Local Storage

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const imagem_enviada = document.querySelector("#imagem_enviada");
 let imagem_carregada = '';
 let acaoBotao = document.getElementById("submeter_informacoes");
 
+
 imagem_enviada.addEventListener("change", function(){
     const leitor = new FileReader();
     leitor.addEventListener("load", () => {
@@ -12,15 +13,40 @@ imagem_enviada.addEventListener("change", function(){
     })
     leitor.readAsDataURL(this.files[0]);
 
+})
+
 acaoBotao.addEventListener("click", function(){
 
-    let dataFoto = document.getElementById("data_foto").value;
-    let textoDescricao = document.getElementById("texto_descricao").value;
-    let tipoPlanta = document.getElementById("tipo_plantacao").value
 
+    let dataFoto = document.getElementById("data_foto");
+    let textoDescricao = document.getElementById("texto_descricao");
+    let tipoPlanta = document.getElementById("tipo_plantacao");
+
+    let dadosLocais = JSON.parse(localStorage.getItem("dados_planta"));
     
+    if(dadosLocais == null){
+        localStorage.setItem("dados_planta", "[]");
+        dadosLocais = [];
+    }
     
-    
-})    
-    
+    let auxRegistro ={ 
+        dataFoto: dataFoto.value,
+        textoDescricao: textoDescricao.value,
+        tipoPlanta: tipoPlanta.value
+    }
+
+    dadosLocais.push(auxRegistro);
+    localStorage.setItem("dados_planta", JSON.stringify(dadosLocais));
+
+    dataFoto.value = "";
+    textoDescricao.value = "";
+    tipoPlanta.value = "";
 })
+
+function preencheCampos(){
+
+    document.getElementById("data_foto").value = JSON.parse(localStorage.getItem("dados_planta"))
+    document.getElementById("texto_descricao").value = JSON.parse(localStorage.auxRegistro.textoDescricao)
+    document.getElementById("tipo_plantacao").value = JSON.parse(localStorage.auxRegistro.tipoPlanta)
+
+}
