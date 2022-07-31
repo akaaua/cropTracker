3bbff550fb0141ffaf9329d43f32cbf006de2629
akaaua <akaaua@gmail.com>
2022-07-31 16:47:40 -0400

    Filtragem por tipo de planta funcionando. Cada página irá exibir seu devido tipo de planta.

--- montaPagina.js
+++ montaPagina.js
@@ -1,47 +1,51 @@
 
-function criaElemento(){
+function criaElemento(tipoElemento){
     dados  = JSON.parse(localStorage.dados_planta);
     let quantidade = 0;
     let localizacaoElemento = document.getElementById('div_tipo')
     
+    for (quantidade = 0; dados.length; quantidade++) {
+        while (tipoElemento == dados[quantidade].tipoPlanta) {
+            let lineBreak = document.createElement("br")
     
-    while (quantidade < dados.length) {
-        let lineBreak = document.createElement("br")
-
-        let novaImagem = document.createElement('img');
-        novaImagem.id = `imagem${quantidade}`
-        novaImagem.src = `${dados[quantidade].urlFoto}`
-        localizacaoElemento.appendChild(novaImagem)
-        localizacaoElemento.appendChild(lineBreak)
-        
-
-        let novaData = document.createElement('input');
-        novaData.id = `data${quantidade}`
-        novaData.type = "date"
-        localizacaoElemento.appendChild(novaData)
-        localizacaoElemento.appendChild(lineBreak)
-        
-
-        let novaDescricao = document.createElement('input');
-        novaDescricao.id = `descricao${quantidade}`
-        novaDescricao.type = 'text'
-        localizacaoElemento.appendChild(novaDescricao)
-        localizacaoElemento.appendChild(lineBreak)
-
-        let novoTipo = document.createElement('input');
-        novoTipo.id = `tipo${quantidade}`
-        novoTipo.type = 'text'
-        localizacaoElemento.appendChild(novoTipo)
-        localizacaoElemento.appendChild(lineBreak)
-
-        preencheDados(quantidade)
-
-        quantidade++
+            let novaImagem = document.createElement('img');
+            novaImagem.id = `imagem${quantidade}`
+            novaImagem.src = `${dados[quantidade].urlFoto}`
+            localizacaoElemento.appendChild(novaImagem)
+            localizacaoElemento.appendChild(lineBreak)
             
+    
+            let novaData = document.createElement('input');
+            novaData.id = `data${quantidade}`
+            novaData.type = "date"
+            localizacaoElemento.appendChild(novaData)
+            localizacaoElemento.appendChild(lineBreak)
+            
+    
+            let novaDescricao = document.createElement('input');
+            novaDescricao.id = `descricao${quantidade}`
+            novaDescricao.type = 'text'
+            localizacaoElemento.appendChild(novaDescricao)
+            localizacaoElemento.appendChild(lineBreak)
+    
+            let novoTipo = document.createElement('input');
+            novoTipo.id = `tipo${quantidade}`
+            novoTipo.type = 'text'
+            localizacaoElemento.appendChild(novoTipo)
+            localizacaoElemento.appendChild(lineBreak)
+    
+            preencheDados(quantidade)
 
+            quantidade++
+    
+            
+    
+        }
+        
     }
     
     
+
 }
 
 // Preenchimento dos campos na tela
